d57490ea8aaaa12d286fbf87640392c780cf3f19
Silvian73 <yurykuz65@gmail.com>
2020-06-26 22:45:00 +0300

    Сделал систему летающих иконок

---
+++ Assets/Scripts/Controllers/FlyingIconsController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Snake_box
+{
+	public class FlyingIconsController : IExecute, IInitialization, ICleanUp
+	{
+        private const float FLYING_ICONS_SPEED = 10;
+
+		private List<FlyingIcon> _flyingIcons = new List<FlyingIcon>();
+
+        #region IExecute
+
+        public void Execute()
+        {
+            float translationDistance = FLYING_ICONS_SPEED * Time.deltaTime;
+            var flyingIcons = _flyingIcons.ToArray();
+            foreach(FlyingIcon icon in flyingIcons)
+            {
+                Vector3 direction = icon.Destination.position - icon.GameObject.transform.position;
+                if(direction.sqrMagnitude <= translationDistance * translationDistance)
+                {
+                    Object.Destroy(icon.GameObject);
+                    _flyingIcons.Remove(icon);
+                }
+                else
+                {
+                    //var translation = (direction.normalized * translationDistance);
+                    //var position = icon.GameObject.transform.position;
+                    //position.x += translation.x;
+                    //position.z += translation.z;
+                    //icon.GameObject.transform.position = position;
+                    icon.GameObject.transform.Translate(direction.normalized * translationDistance, Space.World);
+                }
+
+            }
+        }
+
+        #endregion
+
+        #region IInitializaton
+
+        public void Initialization()
+        {
+            Services.Instance.FlyingIconsService.FlyingIconAdded += AddFlyingIconToList;
+        }
+
+        #endregion
+
+        #region ICleanUp
+
+        public void Clean()
+        {
+            Services.Instance.FlyingIconsService.FlyingIconAdded -= AddFlyingIconToList;
+            foreach (FlyingIcon icon in _flyingIcons)
+            {
+                GameObject.Destroy(icon.gameObject);
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void AddFlyingIconToList(FlyingIcon flyingIcon) => _flyingIcons.Add(flyingIcon);
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Services/FlyingIconsService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Snake_box
+{
+    public class FlyingIconsService
+    {
+        public event Action<FlyingIcon> FlyingIconAdded;
+
+        public void CreateFlyingIcon(Sprite sprite, Vector3 startPosition, Transform destination)
+        {
+            FlyingIcon flyingIcon = new FlyingIcon();
+            flyingIcon.GameObject = new GameObject("Flying Icon", typeof(SpriteRenderer));
+            flyingIcon.Destination = destination;
+            Vector3 position = startPosition;
+            position.y = 1;
+            flyingIcon.GameObject.transform.position = position;
+            var euler = flyingIcon.GameObject.transform.eulerAngles;
+            euler.x = 90;
+            flyingIcon.GameObject.transform.eulerAngles = euler;
+            var spriteRenderer = flyingIcon.GameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = sprite;
+            spriteRenderer.sortingLayerName = "Flying Icon";
+            FlyingIconAdded.Invoke(flyingIcon);
+        }
+    }
+
+}
