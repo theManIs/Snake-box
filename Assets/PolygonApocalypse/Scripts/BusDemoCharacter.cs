8506e3843ea253ae1625a70ec6795f69efbf5c2a
Jim Raynor <lostlose@yandex.ru>
2020-08-12 22:38:50 +0300

    code/artem-m/camera-test

---
+++ Assets/PolygonApocalypse/Scripts/BusDemoCharacter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusDemoCharacter : MonoBehaviour
+{
+    public float BusSpeed = 2f;
+    public float SteerSpeed = 2f;
+    public Camera BusCamera;
+    public float CameraSmoothness = 10;
+
+    private CharacterController _characterController;
+    private float _accumulatedRotation = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+
+        Vector3 localPosition = transform.position;
+        localPosition.y = localPosition.y < 0 ? 0 : localPosition.y;
+        transform.position = localPosition;
+
+        if (BusCamera)
+        {
+            BusCamera.transform.parent = null;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 startTransform = transform.position;
+        Quaternion startRotation = transform.rotation;
+        Vector3 controllerVelocity = transform.forward * Input.GetAxis("Vertical");
+        //        controllerVelocity = controllerVelocity + new Vector3(Input.GetAxis("Vertical"), 0, 0);
+        //        controllerVelocity.z = Input.GetAxis("Horizontal");
+
+        _characterController.Move(controllerVelocity * Time.deltaTime * BusSpeed);
+        _characterController.Move(Vector3.down * Time.deltaTime * BusSpeed);
+
+//        if (controllerVelocity != default && controllerVelocity.x > 0)
+//        {
+////            controllerVelocity.z = Mathf.Lerp(0, controllerVelocity.z, Time.deltaTime * BusSpeed);
+//            transform.forward = controllerVelocity;
+//        }
+
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0)
+        {
+            float absoluteRotation = Time.deltaTime * SteerSpeed * Input.GetAxis("Horizontal");
+            _accumulatedRotation += absoluteRotation;
+
+            transform.Rotate(Vector3.up, absoluteRotation);
+        }
+
+        /*Vector3 localPosition = transform.position;
+        localPosition.y = 0;
+        transform.position = localPosition;*/
+
+        if (BusCamera)
+        {
+            BusCamera.transform.position = BusCamera.transform.position + (transform.position - startTransform);
+            float frameRotationAngle = Time.deltaTime * CameraSmoothness;
+
+            if (Mathf.Abs(_accumulatedRotation) < frameRotationAngle)
+            {
+                _accumulatedRotation = 0;
+            } 
+            else if (_accumulatedRotation > 0)
+            {
+                _accumulatedRotation -= Time.deltaTime * CameraSmoothness;
+
+                BusCamera.transform.Rotate(Vector3.back, frameRotationAngle);
+            } 
+            else if (_accumulatedRotation < 0)
+            {
+                _accumulatedRotation += Time.deltaTime * CameraSmoothness;
+
+                BusCamera.transform.Rotate(Vector3.back, -frameRotationAngle);
+            }
+        }
+    }
+}
